ad64c83277060b2ec37e3a99c22e4c94940b5f62
Chanaka Chathuranga <chanakac.91@gmail.com>
2020-09-08 18:45:30 +0530

    Update greetings.py

--- userbot/plugins/greetings.py
+++ userbot/plugins/greetings.py
@@ -2,10 +2,18 @@ from userbot.utils import admin_cmd
 
 
 @borg.on(admin_cmd(pattern="gnt$"))
-async def gn(event):
+async def gnt(event):
     await event.edit("｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･\n╱╱╱╱╱╱╱╭╮╱╱╱╭╮╱╭╮╭╮\n╭━┳━┳━┳╯┃╭━┳╋╋━┫╰┫╰╮\n┃╋┃╋┃╋┃╋┃┃┃┃┃┃╋┃┃┃╭┫\n┣╮┣━┻━┻━╯╰┻━┻╋╮┣┻┻━╯\n╰━╯╱╱╱╱╱╱╱╱╱╱╰━╯\n｡♥｡･ﾟ♡ﾟ･｡♥° ♥｡･ﾟ♡ﾟ･")
 
 
 @borg.on(admin_cmd(pattern="gmg$"))
-async def gm(event):
+async def gmg(event):
     await event.edit("｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･｡♥｡･ﾟ♡ﾟ･\n╱╱╱╱╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╭╮\n╭━┳━┳━┳╯┃╭━━┳━┳┳┳━┳╋╋━┳┳━╮\n┃╋┃╋┃╋┃╋┃┃┃┃┃╋┃╭┫┃┃┃┃┃┃┃╋┃\n┣╮┣━┻━┻━╯╰┻┻┻━┻╯╰┻━┻┻┻━╋╮┃\n╰━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━╯\n｡♥｡･ﾟ♡ﾟ･｡♥｡･｡･｡･｡♥｡･｡♥｡･ﾟ♡ﾟ･")
+
+@borg.on(admin_cmd(pattern="gm$"))
+async def gm(event):
+    await event.edit("**☆❋──❃ ɢ∞פ ʍօ®ɴɪɴɢ ❃──❋☆**")
+
+@borg.on(admin_cmd(pattern="gn$"))
+async def gn(event):
+    await event.edit(".,-,*´'*°☆\n/.(\n\ { ۰۪۫G۪۫۰۰۪۫O۪۫۰۰۪۫O۪۫۰۰۪۫D۪۫۰ ۰۪۫N۪۫۰۰۪۫I۪۫۰۰۪۫G۪۫۰۰۪۫H۪۫۰۰۪۫T۪۫۰\n-`☆.¸¸.❤️")
